7ae56f01a9e85681d2a9f99219fd75ad7156ebb6
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2023-12-07 12:10:05 +0530

    Added image

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+"use client";
 
+
+
+import Image from "next/image";
+import {motion} from "framer-motion"
 import img from "@/lib/img.jpeg";
 
 function Intro ( ) { 
@@ -8,11 +12,20 @@ function Intro ( ) {
     return ( 
         <section>
             <div className="flex items-center justify-center "> 
-                <div>
-                    <Image src={img} width="192" height="192" alt="Ayush Image"  priority={true} quality={100} className="rounded-full bottom-[0.35rem] object-cover border-white shadow-xl " />
-                    <span className="text-4xl ">
+                <div className="relative ">
+                    <motion.div 
+                    initial={{opacity:0 , scale:0 }}
+                    animate={{opacity:1 , scale:1}}
+                    transition={{delay:0.5}}
+                    >
+                        <Image src={img} width="192" height="192" alt="Ayush Image"  priority={true} quality={100} className="rounded-full bottom-[0.35rem] object-cover border-white  border-2 shadow-xl " />
+                    </motion.div>
+                    <motion.span initial={{opacity:0, scale:0}}
+                    animate={{opacity:1, scale:1}}
+                    transition={{delay:0.5 ,type:"spring", stiffness:260}}
+                    className="absolute  bottom-3 right-1 text-4xl ">
                         👋
-                    </span>
+                    </motion.span>
                 </div>
 
             </div>
